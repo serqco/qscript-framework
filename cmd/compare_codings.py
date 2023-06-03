eb1888f69c5d10f669fd6f988e23956ca70186b9
Lutz Prechelt <prechelt@inf.fu-berlin.de>
2023-06-03 13:19:23 +0200

    qabstracts parts pushed down from compare_codings.py to compare_codings_abs.py

--- cmd/compare_codings.py
+++ cmd/compare_codings.py
@@ -120,24 +120,6 @@ class CodingsComparator:
             self._printextra(self._numbered_sentence(as1), 
                              self._of_1_ok(ctx, as1.annotation), self._of_2_ok(ctx, as2.annotation))
 
-    def check_suffixes(self, annots, as1, as2, ctx, maxcountdiff):
-        for code, counts in annots.codes_with_iucounts(as1.annotation, as2.annotation).items():
-            icount1, ucount1, icount2, ucount2 = counts
-            idiff = abs(icount1 - icount2) > maxcountdiff
-            udiff = abs(ucount1 - ucount2) > maxcountdiff
-            if idiff and udiff:
-                self._printmsg(ctx, f"{code}: Very different numbers of i&u gaps, please reconsider:",
-                               self._numbered_sentence(as1),
-                               self._of_1(ctx, as1.annotation), self._of_2(ctx, as2.annotation))
-            elif idiff:
-                self._printmsg(ctx, f"{code}: Very different numbers of informativeness gaps, please reconsider:",
-                               self._numbered_sentence(as1),
-                               self._of_1(ctx, as1.annotation), self._of_2(ctx, as2.annotation))
-            elif udiff:
-                self._printmsg(ctx, f"{code}: Very different numbers of understandability gaps, please reconsider:",
-                               self._numbered_sentence(as1),
-                               self._of_1(ctx, as1.annotation), self._of_2(ctx, as2.annotation))
-
     def _printmsg(self, ctx: ComparatorContext, msg: str, *items: tg.Sequence[str]):
         """Show sentence with a problem. Suppress header if same as previous."""
         if msg != self.lastmsg:
